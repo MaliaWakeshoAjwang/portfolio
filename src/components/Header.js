28864611c70b620f82020fb837993f8755c08ec7
MaliaWakeshoAjwang <maz.obiri@emory.edu>
2025-01-11 00:50:46 +0000

    Closes #1 - All webiste pages are now responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import styles from '../styles/Header.module.css'
-import styled from'styled-components';
-// import Twitter from '../assets/icons/X.png';
-// import YouTube from '../assets/icons/YouTube.png';
+import styles from '../styles/Header.module.css';
+import styled from 'styled-components';
 import LinkedIn from '../assets/icons/LinkedIn.png';
 import Instagram from '../assets/icons/Instagram.png';
 import Github from '../assets/icons/Github.png';
@@ -18,7 +16,6 @@ const Logo = styled(NavLink)`
     &:hover {
         color: var(--jet);
     }
-
 `;
 
 const Navlink = styled(NavLink)`
@@ -33,72 +30,75 @@ const Navlink = styled(NavLink)`
         color: var(--gold);
         font-weight: bold;
     }
-    
+
     &.active {
         font-weight: 650;
         text-decoration: underline;
         text-decoration-color: var(--gold);
         text-decoration-thickness: 2px;
-        text-underline-offset:5px;
-    }
-
-    &.active::after {
-        content: '';
-        position: absolute;
-        left: 0;
-        bottom: -5px;
-        width: 100%;
-        height: 2px
-        background-color: var(--gold);
+        text-underline-offset: 5px;
     }
 `;
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
                 <Logo to="/">MazWakes</Logo>
             </div>
-            <nav>
+            <div className={styles.hamburger} onClick={toggleMenu}>
+                <span className={menuOpen ? styles.barOpen : styles.bar}></span>
+                <span className={menuOpen ? styles.barOpen : styles.bar}></span>
+                <span className={menuOpen ? styles.barOpen : styles.bar}></span>
+            </div>
+            <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ''}`}>
                 <ul>
                     <li>
-                        <Navlink to="/">Home</Navlink>
+                        <Navlink to="/" onClick={() => setMenuOpen(false)}>Home</Navlink>
                     </li>
                     <li>
-                        <Navlink to="/about">About</Navlink>
+                        <Navlink to="/about" onClick={() => setMenuOpen(false)}>About</Navlink>
                     </li>
                     <li>
-                        <Navlink to="/projects">Projects</Navlink>
+                        <Navlink to="/projects" onClick={() => setMenuOpen(false)}>Projects</Navlink>
                     </li>
-                    {/* <li>
-                        <Navlink to="/blog">Blog</Navlink>
-                    </li> */}
                     <li>
-                        <Navlink to="/contact">Contact</Navlink>
+                        <Navlink to="/contact" onClick={() => setMenuOpen(false)}>Contact</Navlink>
                     </li>
-                    {/* <li>
-                        <a href="https://twitter.com" target="_blank" rel="noopener nore noreferrer" className={styles.x}>
-                            <img src={Twitter} alt='Twitter logo'/>
-                        </a>
-                    </li> */}
                     <li>
-                        <a href="https://www.instagram.com/mazwakes/" target="_blank" rel="noopener nore noreferrer" className={styles.insta}>
-                            <img src={Instagram} alt='Instagram logo'/>
-                        </a>
-                    </li>
-                    {/* <li>
-                        <a href="https://www.youtube.com" target="_blank" rel="noopener nore noreferrer" className={styles.yt}>
-                            <img src={YouTube} alt='YouTube logo'/>
-                        </a>
-                    </li> */}
-                    <li>
-                        <a href="https://www.linkedin.com/in/malia-wakesho-ajwang/" target="_blank" rel="noopener nore noreferrer" className={styles.linkedin}>
-                            <img src={LinkedIn} alt='LinkedIn logo'/>
+                        <a
+                            href="https://www.instagram.com/mazwakes/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.insta}
+                        >
+                            <img src={Instagram} alt='Instagram logo' />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/MaliaWakeshoAjwang" target="_blank" rel="noopener nore noreferrer" className={styles.github}>
-                            <img src={Github} alt='Github logo'/>
+                        <a
+                            href="https://www.linkedin.com/in/malia-wakesho-ajwang/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.linkedin}
+                        >
+                            <img src={LinkedIn} alt='LinkedIn logo' />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://github.com/MaliaWakeshoAjwang"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.github}
+                        >
+                            <img src={Github} alt='Github logo' />
                         </a>
                     </li>
                 </ul>
